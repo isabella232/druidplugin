49de842bd3d36b5fe8a0a17eacc0fa8610162d3c
Victor Trikolenko <victor.trikolenko@liquidm.com>
2019-09-03 12:34:40 +0200

    Fix metricFindQuery

--- dist/datasource.ts
+++ dist/datasource.ts
@@ -316,7 +316,7 @@ export default class DruidDatasource {
       intervals = this.getQueryIntervals(from, to);
 
     var q = JSON.parse(this.templateSrv.replace(query));
-    if (q.filter.fields) q.filter.fields = q.filter.fields.filter(f => f.value && f.value !== 'skipFilter');
+    if (q.filter.fields) q.filter.fields = q.filter.fields.filter(f => !f.value || f.value !== 'skipFilter');
     q.intervals = intervals;
 
     return this.druidQuery(q)
